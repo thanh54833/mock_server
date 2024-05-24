3188620e705f2c7ca7642d60c8699f9604ea9b8f
thanh.ph <phamhoaithanh32@gmail.com>
2024-05-24 07:26:37 +0700

    push code

---
+++ Makefile
@@ -0,0 +1,5 @@
+
+
+
+p_c:
+	git add --all && git commit -m "push code" && git push
--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-from fastapi import FastAPI
+import json
+
+from fastapi import FastAPI, Request
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.staticfiles import StaticFiles
+from fastapi.openapi.utils import get_openapi
+import os
 
 app = FastAPI()
 
+# Mount the Swagger UI static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# ... (your existing API endpoints)
 
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html(request: Request):
+    openapi_url = "/openapi.json"
+    return get_swagger_ui_html(
+        openapi_url=openapi_url,
+        title="My API - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        init_oauth=app.swagger_ui_init_oauth,
+    )
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/openapi.json", include_in_schema=False)
+async def get_open_api_endpoint():
+    with open(os.path.join("mock_server", "openapi.json"), "r") as f:
+        return json.load(f)
