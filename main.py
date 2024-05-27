c0b64e37464c3ef642663bc3802f9613d201473d
thanh.ph <phamhoaithanh32@gmail.com>
2024-05-27 08:45:10 +0700

    push code

--- main.py
+++ main.py
@@ -5,5 +5,3 @@ from starlette.staticfiles import StaticFiles
 app = FastAPI()
 app.mount("/images/avif", StaticFiles(directory="images/avif"), name="images")
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8002)
