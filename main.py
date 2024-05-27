e9242243c5911c8e50a02a4ccfd07589584844c1
thanh.ph <phamhoaithanh32@gmail.com>
2024-05-27 09:27:25 +0700

    push code

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-import uvicorn
+import os
+
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
-app.mount("/images/avif", StaticFiles(directory="images/avif"), name="images")
 
+
+def is_running_in_docker():
+    return os.path.exists('/.dockerenv')
+
+
+if is_running_in_docker():
+    app.mount("/images/avif", StaticFiles(directory="/app/images/avif"), name="images")
+else:
+    app.mount("/images/avif", StaticFiles(directory="images/avif"), name="images")
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
