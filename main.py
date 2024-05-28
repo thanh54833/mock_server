7d4dc3c318d1986601a55783d492bdb587496ae9
thanh.ph <phamhoaithanh32@gmail.com>
2024-05-28 17:44:40 +0700

    push code

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import subprocess
-
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
 
@@ -10,7 +8,6 @@ app = FastAPI()
 app.include_router(home_cache_api, prefix="/home_cache_api")
 
 app.mount("/images", StaticFiles(directory="images/avif"), name="images")
-
 
 @app.on_event("startup")
 def startup_event():
