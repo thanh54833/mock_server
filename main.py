83d086f0a36bafc77260a99f38e7b7ff80975da3
thanh.ph <phamhoaithanh32@gmail.com>
2024-05-28 17:08:36 +0700

    push code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
 
@@ -8,6 +10,14 @@ app = FastAPI()
 app.include_router(home_cache_api, prefix="/home_cache_api")
 
 app.mount("/images", StaticFiles(directory="images/avif"), name="images")
+
+
+@app.on_event("startup")
+def startup_event():
+    #print("Starting up...")
+    #subprocess.run(["python", "add_tag_openapi.py"], check=True)
+    subprocess.run(["python", "png_to_avif.py"], check=True)
+    print("Starting up...")
 
 
 @app.get("/")
