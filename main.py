45254ce8d40d4603f09cea918e45a351714ad43e
thanh.ph <phamhoaithanh32@gmail.com>
2024-06-05 19:55:21 +0700

    push code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import subprocess
+import os
 
 from fastapi import FastAPI
+from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 
 from cache.home_cache_api import home_cache_api
@@ -12,23 +13,15 @@ app.include_router(home_cache_api, prefix="/home_cache_api")
 app.mount("/images", StaticFiles(directory="images/webp"), name="images")
 
 
-@app.on_event("startup")
-def startup_event():
-    # print("Starting up...")
-    # subprocess.run(["python", "add_tag_openapi.py"], check=True)
-    # subprocess.run(["python3", "image_to_avif.py"], check=True)
-    # subprocess.run(["python", "download_image.py"], check=True)
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    # Get a list of all files in the /images directory
+    images = os.listdir("images/webp/")
 
-    # Add all changes to the staging area
-    # subprocess.run(["git", "add", "--all"], check=True)
-    # Commit the changes with a message
-    # subprocess.run(["git", "commit", "-m", "+ auto push code"], check=True)
-    # Push the changes to the 'main' branch on the 'origin' remote
-    # subprocess.run(["git", "push", "origin", "HEAD:main"], check=True)
+    # Create an HTML page with an <img> tag for each image
+    html_content = "<html><body>"
+    for image in images:
+        html_content += f'<img src="/images/{image}" alt="{image}" style="width: 200px; height: auto;"><br>'
+    html_content += "</body></html>"
 
-    print("Starting up...")
-
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+    return html_content
